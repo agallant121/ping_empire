3cc7388d86026b203b8b8f3e185989c4b7944eb7
Alex Gallant <agallant1201@gmail.com>
2025-07-12 14:49:01 -0400

    Add sidekiq to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
-
+  mount Sidekiq::Web => "/sidekiq"
+  
   namespace :api do
     namespace :v1 do
       resources :websites, only: [:create, :index, :show, :destroy]

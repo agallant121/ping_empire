1669ddbdb97e4e39da7dfa9b6a082d5f08a40f85
Alex Gallant <agallant1201@gmail.com>
2025-07-12 15:32:52 -0400

    Update to use correct file name sidekiq_scheduler

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,10 @@
 Sidekiq.configure_server do |config|
-  config.on(:startup) do
-    schedule_file = Rails.root.join("config/sidekiq.yml")
-    if File.exist?(schedule_file)
-      Sidekiq::Scheduler.dynamic = true
-      Sidekiq.schedule = YAML.load_file(schedule_file)
+  schedule_file = "config/sidekiq_scheduler.yml"
+
+  if File.exist?(schedule_file)
+    Sidekiq::Scheduler.dynamic = true
+    config.on(:startup) do
+      Sidekiq.schedule = YAML.load_file(schedule_file)[:schedule]
       Sidekiq::Scheduler.reload_schedule!
     end
   end

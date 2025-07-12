d192705bb00091b79c44d5de2d38b4c146bec488
Alex Gallant <agallant1201@gmail.com>
2025-07-12 09:55:13 -0400

    Remove duplicate attribute response_time

--- db/migrate/20250712135203_create_responses.rb
+++ db/migrate/20250712135203_create_responses.rb
@@ -2,7 +2,6 @@ class CreateResponses < ActiveRecord::Migration[8.0]
   def change
     create_table :responses do |t|
       t.references :website, null: false, foreign_key: true
-      t.integer :response_time
       t.integer :status_code
       t.float :response_time
       t.datetime :checked_at
